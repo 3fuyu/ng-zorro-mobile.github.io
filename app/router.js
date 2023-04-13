7b4e2478b50e0232acd830c739a8531252139cf0
3fuyu <banxianyu@gmail.com>
2023-04-14 00:52:58 +0800

    feat(update): start

--- app/router.js
+++ app/router.js
@@ -13,18 +13,18 @@ module.exports = app => {
   router.get('/openAi', controller.openAi.openAi);
   router.get('/openAiChat', controller.openAi.openAiChat);
 
-  router.post('/openai/createFineTune', controller.openAi.createFineTune);
-  router.get('/openai/listFineTunes', controller.openAi.listFineTunes);
-  router.get('/openai/listFineTuneEvents', controller.openAi.listFineTuneEvents);
-  router.get('/openai/retrieveFineTune', controller.openAi.retrieveFineTune);
-  router.post('/openai/cancelFineTune', controller.openAi.cancelFineTune);
-  router.post('/openai/deleteFineTune', controller.openAi.deleteFineTune);
+  router.post('/createFineTune', controller.openAi.createFineTune);
+  router.get('/listFineTunes', controller.openAi.listFineTunes);
+  router.get('/listFineTuneEvents', controller.openAi.listFineTuneEvents);
+  router.get('/retrieveFineTune', controller.openAi.retrieveFineTune);
+  router.post('/cancelFineTune', controller.openAi.cancelFineTune);
+  router.post('/deleteFineTune', controller.openAi.deleteFineTune);
 
-  router.get('/openai/listFiles', controller.openAi.listFiles);
-  router.post('/openai/uploadFile', controller.openAi.uploadFile);
-  router.post('/openai/deleteFile', controller.openAi.deleteFile);
-  router.get('/openai/retrieveFile', controller.openAi.retrieveFile);
-  router.get('/openai/retrieveFileContent', controller.openAi.retrieveFileContent);
+  router.get('/listFiles', controller.openAi.listFiles);
+  router.post('/uploadFile', controller.openAi.uploadFile);
+  router.post('/deleteFile', controller.openAi.deleteFile);
+  router.get('/retrieveFile', controller.openAi.retrieveFile);
+  router.get('/retrieveFileContent', controller.openAi.retrieveFileContent);
 
-  router.post('/openai/createEmbedding', controller.openAi.createEmbedding);
+  router.post('/createEmbedding', controller.openAi.createEmbedding);
 };
--- config/config.default.js
+++ config/config.default.js
@@ -22,6 +22,12 @@ module.exports = appInfo => {
     prefix: '/assets/',
     dir:[path.join(__dirname, '../app/assets')]
   };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
   // config.static => 在更新证书验证阶段，需要使用well-known，在日常运行阶段 需要用assets 注意在更新证书阶段和更新完成后修改
   // config.static = {
   //   prefix: '/.well-known/',
